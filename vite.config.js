4c8602ac77d85c588ca67b8735d1eb8ef5219585
Alvarenga20 <moroni.ayres1@hotmail.com>
2024-11-17 19:51:42 -0300

    vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,19 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import path from 'path';
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, 'src'),
+    },
+  },
+  build: {
+    rollupOptions: {
+      external: [
+        '@fortawesome/fontawesome-free/css/all.min.css', // Explicitly externalize FontAwesome CSS
+      ],
+    },
+  },
+});
